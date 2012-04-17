2948e98626702b4302803c8738dcbb51a9b4f55c
Masashi Iizuka <liquidz.uo@gmail.com>
2012-04-17 22:27:19 +0900

    add :main

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject debug-reader "0.0.1"
   :description "Gauche like debug reader"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main debug-reader.core)
